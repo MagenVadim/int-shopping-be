00b67e7e59ea52a57eca5fd107d05ab03a3a048c
Vadim Smagin <magenvadim@gmail.com>
2026-01-13 15:55:32 +0200

    sending first request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,1 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+
+@Controller("/api")
+export class AppController {
+  @Get("/users")
+  getUsers() {
+    return [
+      { id: 1, name: "John Doe" },
+      { id: 2, name: "Jane Smith" },
+    ];
+  }
+}
