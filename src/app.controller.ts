3e88ca7d413884ce67acf30a90fbd558901a3505
Vadim Smagin <magenvadim@gmail.com>
2026-01-14 12:33:07 +0200

    AppService has added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller("/api")
 export class AppController {
+  constructor(private appService: AppService) {}
+
   @Get("/users")
   getUsers() {
-    return [
-      { id: 1, name: "John Doe" },
-      { id: 2, name: "Jane Smith" },
-    ];
+    return this.appService.getUsers();
   }
 }
