efc09eface8048b1b24edfe9ecac9e71a0afd397
Vadim Smagin <magenvadim@gmail.com>
2026-01-23 17:26:09 +0200

    CreateUserDto has created

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { User } from "./users.model";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+  async createUser(dto: CreateUserDto) {}
+  async getAllUsers() {}
+}
