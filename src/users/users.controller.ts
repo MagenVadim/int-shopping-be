27af4488b5e513684352ff4e54342997914faed2
Vadim Smagin <magenvadim@gmail.com>
2026-01-26 18:14:25 +0200

    UsersController has updated

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,13 +2,18 @@ import { Body, Controller, Get, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { User } from "./users.model";
 
 @Controller("users")
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @ApiOperation({ summary: "Create user" })
-  @ApiResponse({ status: 200, description: "User created successfully." })
+  @ApiResponse({
+    status: 200,
+    description: "User created successfully.",
+    type: User,
+  })
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
@@ -18,6 +23,7 @@ export class UsersController {
   @ApiResponse({
     status: 200,
     description: "List of users retrieved successfully.",
+    type: User,
   })
   @Get()
   getAll() {
