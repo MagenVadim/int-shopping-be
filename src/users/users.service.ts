21816a2acd55963a4876c53e2a0ce87e42e9f6db
Vadim Smagin <magenvadim@gmail.com>
2026-01-26 14:06:07 +0200

    Controllers has connected

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,6 +7,12 @@ import { CreateUserDto } from "./dto/create-user.dto";
 export class UsersService {
   constructor(@InjectModel(User) private userRepository: typeof User) {}
 
-  async createUser(dto: CreateUserDto) {}
-  async getAllUsers() {}
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto);
+    return user;
+  }
+  async getAllUsers() {
+    const users = await this.userRepository.findAll();
+    return users;
+  }
 }
