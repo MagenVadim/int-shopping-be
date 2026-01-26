556409f2a0798c2bd01c5fed8291cd8d1b2e3344
Vadim Smagin <magenvadim@gmail.com>
2026-01-26 18:08:09 +0200

    Model has updated

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface UserCreationAttrs {
@@ -7,6 +8,7 @@ interface UserCreationAttrs {
 
 @Table({ tableName: "users" })
 export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({ example: 1, description: "Unique user ID" })
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -15,15 +17,19 @@ export class User extends Model<User, UserCreationAttrs> {
   })
   declare id: number;
 
+  @ApiProperty({ example: "abc@gmail.com", description: "User email address" })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
 
+  @ApiProperty({ example: "strongPassword123", description: "User password" })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 
+  @ApiProperty({ example: false, description: "Is user banned?" })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
 
+  @ApiProperty({ example: "Violation of terms", description: "Reason for ban" })
   @Column({ type: DataType.BOOLEAN, allowNull: true })
   banReason: string;
 }
