b9491ffe4df4502628f4e295fa5db58ae2cf8e62
Vadim Smagin <magenvadim@gmail.com>
2026-01-27 17:12:23 +0200

    Roles features has implemented

---
+++ src/roles/roles.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { RolesController } from "./roles.controller";
+import { RolesService } from "./roles.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Role } from "./roles.model";
+import { User } from "src/users/users.model";
+import { UserRoles } from "./user-roles.model";
+
+@Module({
+  providers: [RolesService],
+  controllers: [RolesController],
+  imports: [SequelizeModule.forFeature([User, Role, UserRoles])],
+})
+export class RolesModule {}
